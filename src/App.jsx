d4c64a143c3b21eaa9675490b4152f7bc925bcfc
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-07-27 19:52:59 -0500

    First commit PocketBase login

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Login from "~/components/Login";
+
+function App() {
+  return (
+    <main className="flex justify-center items-center h-screen dark:bg-black">
+      <Login />
+    </main>
+  );
+}
+
+export default App;
